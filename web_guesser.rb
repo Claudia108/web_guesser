b753cff2c909b62792455e414d0549c479d92c0d
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-03-14 14:31:55 -0600

    Extension: background color completed

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 get '/' do
   answer = params["guess"]
   message = check_answer(answer)
-  erb:index, :locals => {:message => message}
+  color = colors(answer)
+  erb :index, :locals => {:message => message, :color => color}
 end
 
-SECRET_NUMBER = 50
+SECRET_NUMBER = rand(100)
 
 def check_answer(answer)
   if answer.to_i > (SECRET_NUMBER + 5)
@@ -19,7 +21,20 @@ def check_answer(answer)
   elsif answer.to_i < SECRET_NUMBER
     "Too low!"
   else
-    "You got it right!\n\nThe secret number is #{SECRET_NUMBER}"
+    "You got it right!\nThe secret number is #{SECRET_NUMBER}"
   end
+end
 
+def colors(answer)
+  if answer.to_i > (SECRET_NUMBER + 5)
+    color = "#a82525"
+  elsif answer.to_i < (SECRET_NUMBER - 5)
+    color = "#a82525"
+  elsif answer.to_i > SECRET_NUMBER
+    color = "#e67676"
+  elsif answer.to_i < SECRET_NUMBER
+    color = "#e67676"
+  else
+    color = "#43801d"
+  end
 end
